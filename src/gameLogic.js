2b04d6bc281f6a81427ec23e1ef529443d0be1a9
keynoteroom1234 <keynoteroom@gmail.com>
2025-05-14 23:23:12 -0700

    Create gameLogic.js file with Game export

---
+++ src/gameLogic.js
@@ -0,0 +1,16 @@
+// Game object with basic structure
+export const Game = {
+    currentPlayerId: null,
+    
+    initializeGame() {
+        console.log('Game initialized');
+    },
+
+    toggleGridInventoryModal() {
+        console.log('Toggle inventory modal');
+    },
+
+    sendChatMessage(message) {
+        console.log('Chat message:', message);
+    }
+};
